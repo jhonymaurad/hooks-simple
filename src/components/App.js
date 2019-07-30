609695d877288b04f18795044e1d0ad5b8314978
jhony maurad <jhonymaurad@gmail.com>
2019-07-29 22:29:22 -0400

    Incorporated componentDidUpdate

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,17 @@
 import React, {useState } from 'react';
+import ResourceList from './ResourceList'
 
 const App =() => {
-
+    //[currentValue, setCurrentValue] = useState(initialValue)
     const [resource, setResource] = useState('posts');
+    //another example: const [currentCount, setCount] = useState(0);
     return(
         <div>
             <div>
                 <button onClick={()=> setResource('posts')}>Posts</button>
                 <button onClick={()=> setResource('todos')}>Todos</button>
             </div>
-            {resource}
+            <ResourceList resource={resource}/>
         </div>
     )
     
